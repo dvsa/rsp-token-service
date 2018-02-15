77a78d41af5a2cec7b1c58c972dd9a3fec1bb7b9
seanmcneilly2 <33751736+seanmcneilly2@users.noreply.github.com>
2018-02-15 10:56:05 +0000

    Adding further error handling and logging

--- src/services/decryptService.js
+++ src/services/decryptService.js
@@ -12,9 +12,11 @@ export default class Notify {
 	static decrypt(token, callback) {
 		if (!TokenValidator(token)) {
 			Notify.ErrorResponse(callback);
+			console.log('Token failed validation');
 			return;
 		} else if (!TokenValidator(teaPass)) {
 			Notify.IncorrectPassFormat(callback);
+			console.log('Pass failed validation');
 			return;
 		}
 
@@ -22,11 +24,19 @@ export default class Notify {
 		const teaPassArray = new Uint32Array(ConvertHex.hexToBytes(teaPass));
 		const uint32Token = new Uint32Array(tokenByteArray.buffer);
 
-		const decryptedVal = DecryptTea(uint32Token, teaPassArray);
-		const penaltyItems = ParseDecryptedToken(decryptedVal);
+		let penaltyItems = '';
+		try {
+			const decryptedVal = DecryptTea(uint32Token, teaPassArray);
+			penaltyItems = ParseDecryptedToken(decryptedVal);
+		} catch (error) {
+			Notify.ErrorResponse(callback);
+			console.log(error);
+			return;
+		}
 
 		if (penaltyItems === '') {
 			Notify.IncorrectTokenFormatResponse(callback);
+			console.log('Token has decrypted in the incorrect format');
 			return;
 		}
 
