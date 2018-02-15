9efff1e04a38ebcaec325ab036d9a350858395e3
seanmcneilly2 <33751736+seanmcneilly2@users.noreply.github.com>
2018-02-15 15:13:14 +0000

    Preserving leading 0's in Immobilisation reference number

--- src/utils/parseDecryptedToken.js
+++ src/utils/parseDecryptedToken.js
@@ -26,11 +26,12 @@ export default (decryptedToken) => {
 
 	const paymentAmount = parseInt(binaryArray.slice(4, 18).join(''), 2);
 	const docType = parseInt(binaryArray.slice(18, 20).join(''), 2);
-	const parsedRef = parseInt(binaryArray.slice(20, 64).join(''), 2);
+	let parsedRef = parseInt(binaryArray.slice(20, 64).join(''), 2);
 
 	let ref = parsedRef.toString();
 	if (docType === 1) {
 		ref = '0000000000000'.slice(ref.length) + ref;
+		parsedRef = ref;
 		const splitRef = ref.split('');
 		const section1 = parseInt(splitRef.slice(0, 6).join(''), 10);
 		const section2 = parseInt(splitRef.slice(6, 7).join(''), 10);
