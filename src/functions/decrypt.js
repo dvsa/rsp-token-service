6febe333c0832da33cb5c2f3bf4152ad5990979f
seanmcneilly2 <33751736+seanmcneilly2@users.noreply.github.com>
2018-02-15 15:28:30 +0000

    Adding incoming request log

--- src/functions/decrypt.js
+++ src/functions/decrypt.js
@@ -1,6 +1,7 @@
 import DecryptService from '../services/decryptService';
 
 export default (event, context, callback) => {
+	console.log(event.body);
 	const decryptObject = JSON.parse(event.body);
 
 	DecryptService.decrypt(decryptObject.Token, callback);
