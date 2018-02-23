6f1f44b4b133e3b539da11ba0bfdc39b47800d78
seanmcneilly2 <33751736+seanmcneilly2@users.noreply.github.com>
2018-02-23 16:07:58 +0000

    Refactoring ishex return

--- src/utils/isHex.js
+++ src/utils/isHex.js
@@ -1,9 +1,5 @@
 const hexRegex = /^(0x|0X)?[a-fA-F0-9]+$/;
 
 export default (hexString) => {
-	const result = hexString.match(hexRegex);
-	if (result || result !== null) {
-		return true;
-	}
-	return false;
+	return hexString.match(hexRegex) != null;
 };
