7bdefe874f1f2c69cdfbf20dbe29852c982ab423
Fabio Simao <fabio.simao@bjss.com>
2018-03-08 18:39:30 +0000

    Adds couple of fixes for validation rules

--- src/utils/checkFormat.js
+++ src/utils/checkFormat.js
@@ -2,6 +2,5 @@ import IsHex from '../utils/isHex';
 
 export default (token) => {
 	// This util will check the basic format of the token.
-
-	return token.length === 16 || IsHex(token);
+	return token.length === 16 && IsHex(token);
 };
