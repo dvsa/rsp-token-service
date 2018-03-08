f039ba563f8e5561396f254e09153dfb0812a55b
seanmcneilly2 <33751736+seanmcneilly2@users.noreply.github.com>
2018-03-08 15:24:48 +0000

    Logic fix for length of token

--- src/utils/checkFormat.js
+++ src/utils/checkFormat.js
@@ -3,5 +3,5 @@ import IsHex from '../utils/isHex';
 export default (token) => {
 	// This util will check the basic format of the token.
 
-	return token.length !== 16 || IsHex(token);
+	return token.length === 16 || IsHex(token);
 };
