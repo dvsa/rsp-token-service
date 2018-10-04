ecdfc33ff270c37fb0accc7c2b6802b1ed12ece9
John Morley <john.morley@bjss.com>
2018-10-04 16:43:49 +0100

    Allow config to be sourced from AWS Secrets Manager

--- src/functions/decrypt.js
+++ src/functions/decrypt.js
@@ -1,6 +1,13 @@
+import 'babel-polyfill';
 import DecryptService from '../services/decryptService';
+import config from '../config';
 
-export default (event, context, callback) => {
+let configBootstrapped = false;
+export default async (event, context, callback) => {
+	if (!configBootstrapped) {
+		await config.bootstrap();
+		configBootstrapped = true;
+	}
 	console.log(JSON.stringify(event, null, 2));
 	let decryptObject = event.body;
 	if (typeof decryptObject === 'string') {
